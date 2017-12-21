50f5294dde7ee9403b22c52c57f4e0a88a303b26
stopvoldemort <david.squires1@gmail.com>
2017-12-21 10:45:49 -0500

    added message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -15,8 +15,8 @@ export class MessageService {
 
   reverse(string: string): string {
     let newString = ""
-    for (let i = string.length; i > 0; i--) {
-      newString + string[i]
+    for (let i = string.length - 1; i > 0; i--) {
+      newString = newString + string[i]
     }
     return newString
   }
--- src/app/state.service.ts
+++ src/app/state.service.ts
@@ -3,12 +3,18 @@ import { State } from './state'
 import { STATES } from './states'
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service'
+
 
 @Injectable()
 export class StateService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
   getStates(): Observable<State[]> {
+    const str = this.messageService.reverse("StateService: Fetched State")
+    this.messageService.add(str)
+    console.log("StateService this: ", this)
     return of(STATES)
   }
 
